f1316ab9aa0cd7d909fc957953248a8974047bf9
Alfonso Socarras <asocarras10@gmail.com>
2019-08-26 18:53:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,13 +24,7 @@ def read_from_hash(hash, key)
   hash[key]
 end
 
-def update_counting_hash(hash, key)
-  
- if update_counting_hash(hash[key])
-   update_counting_hash(hash[key] += 1)
- else
-   update_counting_hash(hash[key] = 1)
- end
+
    
    
 end
